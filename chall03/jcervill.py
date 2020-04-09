c00a04be25af5b1d395fc55f59a10e6cdf9cfd38
javier <javier@iMac-de-javier.local>
2020-04-09 21:47:23 +0200

    more pythonic

--- chall03/jcervill.py
+++ chall03/jcervill.py
@@ -11,27 +11,18 @@ def snail(mtx):
 	row_end = len(mtx) - 1
 	col_start = 0
 	col_end = len(mtx[0]) - 1
-	str = ''
 	while (row_start <= row_end and col_start <= col_end):
-		for i in range(col_start, col_end + 1):
-			if (row_start == row_end): str += mtx[row_start][i]
-			else: str += mtx[row_start][i] + ", "
+		if row_end == row_start or col_end == col_start: ending = ""
+		else: ending = ", "
+		print(", ".join(mtx[row_start][i] for i in range(col_start, col_end + 1)), end = ending)
 		row_start += 1
-		for i in range(row_start, row_end + 1):
-			if col_start == col_end: str += mtx[i][col_end]
-			else: str += mtx[i][col_end] + ", "
+		print(", ".join(mtx[i][col_end] for i in range(row_start, row_end + 1)), end = ending)
 		col_end -= 1
-		if row_end >= row_start:
-			for i in range(col_end, col_start - 1, -1):
-				if row_end == row_start: str += mtx[row_end][i]
-				else: str += mtx[row_end][i] + ", "
+		if row_end >= row_start: print(", ".join(mtx[row_end][i] for i in range(col_end, col_start - 1, -1)), end = ending)
 		row_end -= 1
-		if col_end >= col_start:
-			for i in range(row_end, row_start - 1, -1):
-				if col_end == col_start: str += mtx[i][col_start]
-				else: str += mtx[i][col_start] + ", "
+		if col_end >= col_start: print(", ".join(mtx[i][col_start] for i in range(row_end, row_start - 1, -1)), end = ending)
 		col_start += 1
-	print(str)
+
 def main():
 	args = len(sys.argv)
 	matrix = []
