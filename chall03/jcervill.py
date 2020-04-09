b14bb408bf997a4e254c93057b1d3f262fdb2419
javier <javier@iMac-de-javier.local>
2020-04-09 23:01:56 +0200

    goin backforward

--- chall03/jcervill.py
+++ chall03/jcervill.py
@@ -6,36 +6,35 @@ def error():
 	print('usage: ' + sys.argv[0] + ' <1-9 squared_rows...>')
 	exit(1)
 
+def ending_function(rw_end,rw_st,c_end,c_st):
+	if rw_end <= rw_st  and c_end <= c_st: return ""
+	else: return ", "
+
 def snail(mtx):
 	row_start = 0
 	row_end = len(mtx) - 1
 	col_start = 0
 	col_end = len(mtx[0]) - 1
-	str = ''
 	while (row_start <= row_end and col_start <= col_end):
-		for i in range(col_start, col_end + 1):
-			if (row_start == row_end): str += mtx[row_start][i]
-			else: str += mtx[row_start][i] + ", "
+		ending = ending_function(row_end, row_start, col_end, col_start)
+		print(", ".join(mtx[row_start][i] for i in range(col_start, col_end + 1)), end = ending)
 		row_start += 1
-		for i in range(row_start, row_end + 1):
-			if col_start == col_end: str += mtx[i][col_end]
-			else: str += mtx[i][col_end] + ", "
+		ending = ending_function(row_end, row_start, col_end, col_start)
+		print(", ".join(mtx[i][col_end] for i in range(row_start, row_end + 1)), end = ending)
 		col_end -= 1
 		if row_end >= row_start:
-			for i in range(col_end, col_start - 1, -1):
-				if row_end == row_start: str += mtx[row_end][i]
-				else: str += mtx[row_end][i] + ", "
+			ending = ending_function(row_end, row_start, col_end, col_start)
+			print(", ".join(mtx[row_end][i] for i in range(col_end, col_start - 1, -1)), end = ending)
 		row_end -= 1
 		if col_end >= col_start:
-			for i in range(row_end, row_start - 1, -1):
-				if col_end == col_start: str += mtx[i][col_start]
-				else: str += mtx[i][col_start] + ", "
+			ending = ending_function(row_end, row_start, col_end, col_start)
+			print(", ".join(mtx[i][col_start] for i in range(row_end, row_start - 1, -1)), end = ending)
 		col_start += 1
-	print(str)
+
 def main():
 	args = len(sys.argv)
 	matrix = []
-	if  args in range (2,22):
+	if args in range(2, 22):
 		for fila in range(1,args):
 			if len(sys.argv[fila]) != args - 1 or sys.argv[fila].isnumeric() == False: error()
 			else:
